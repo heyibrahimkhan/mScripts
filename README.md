32eb209009881bcde2d9dbc5b59b42f1fffa87d7
Ibrahim Ali Khan <heyibrahimkhan@gmail.com>
2017-07-31 23:52:49 -0700

    Screenshot.ps1 added. README.md updated
    Signed-off-by: Ibrahim Ali Khan <heyibrahimkhan@gmail.com>

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 My repository containing useful scripts.
 This'll grow with the years
+
+1. Powershell Scripts:
+	1. Screenshots.ps1: Alt+Tabs to directly next window and screenshots all pages while pressing Page Down
---
+++ ScreenShot.ps1
@@ -0,0 +1,22 @@
+﻿[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+}
+
+[System.Windows.Forms.SendKeys]::SendWait("%{TAB}")
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1000, 900)
+For ($i=0; $i -le 1000; $i++) {
+    sleep 1
+    $bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1600, 900)
+    screenshot $bounds ("D:\SS\screenshot" + ($i -as [string]) + ".png")
+    [System.Windows.Forms.SendKeys]::SendWait("^{PGDN}")
+    echo $i
+}
